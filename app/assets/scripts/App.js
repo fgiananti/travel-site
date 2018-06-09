633e2f070430b34c5412cf3bace2cbb6b01f8fef
francesco <fgiananti@gmail.com>
2018-06-09 18:33:04 +0200

    revel on scroll v1

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 const mobileMenu = new MobileMenu();
+new RevealOnScroll(document.querySelectorAll('.feature-item'), '85%');
+new RevealOnScroll(document.querySelectorAll('.testimonial'), '70%');
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,31 @@
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(elems, offset) {
+    this.itemsToReveal = elems;
+    this.offsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.forEach(item => {
+      item.classList.add('reveal-item');
+    });
+  }
+
+  createWaypoints() {
+    const that = this;
+    this.itemsToReveal.forEach(function(item) {
+      new Waypoint({
+        element: item,
+        handler: () => {
+          item.classList.add('reveal-item--is-visible');
+        },
+        offset: that.offsetPercentage
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
